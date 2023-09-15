b971e81ef352430a3edbd3e717c6fc3ec28d93fa
ErlingMMM <emagnusmoland@gmail.com>
2023-09-15 13:00:02 +0200

    added using import

--- Hero/Program.cs
+++ Hero/Program.cs
@@ -36,10 +36,10 @@ class Program
 
 
 
-        Armor headArmor = new Armor("Head Armor", 1, Slot.Head, ArmorType.Mail, new Dungeon.Attributes.HeroAttribute(10, 20, 30));
-        Armor headArmor2 = new Armor("Head Armor2", 1, Slot.Head, ArmorType.Mail, new Dungeon.Attributes.HeroAttribute(10, 20, 30));
-        Armor clothArmor = new Armor("Head Armor", 1, Slot.Head, ArmorType.Cloth, new Dungeon.Attributes.HeroAttribute(10, 20, 30));
-        Armor legArmor = new Armor("Leg Armor", 1, Slot.Legs, ArmorType.Leather, new Dungeon.Attributes.HeroAttribute(10, 20, 30));
+        Armor headArmor = new Armor("Head Armor", 1, Slot.Head, ArmorType.Mail, new HeroAttribute(10, 20, 30));
+        Armor headArmor2 = new Armor("Head Armor2", 1, Slot.Head, ArmorType.Mail, new HeroAttribute(10, 20, 30));
+        Armor clothArmor = new Armor("Head Armor", 1, Slot.Head, ArmorType.Cloth, new HeroAttribute(10, 20, 30));
+        Armor legArmor = new Armor("Leg Armor", 1, Slot.Legs, ArmorType.Leather, new HeroAttribute(10, 20, 30));
 
 
         archer.EquipArmor(headArmor);
