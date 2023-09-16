c9b09defb5cad693161197818b0bda1f0d1c098f
ErlingMMM <emagnusmoland@gmail.com>
2023-09-16 13:59:34 +0200

    ArcherIncreaseLevel_ShouldHaveCorrectIncreasedLevelAttributes

--- DungeMaster.Tests/HeroUnitTests.cs
+++ DungeMaster.Tests/HeroUnitTests.cs
@@ -1,4 +1,5 @@
 using DungeonMaster.hero;
+using NUnit.Framework.Internal;
 
 namespace DungenMaster.Tests
 {
@@ -18,6 +19,25 @@ namespace DungenMaster.Tests
 
 
             Assert.Equal(expectedName, archer.GetName());
+            Assert.Equal(expectedLevel, archer.Level);
+            Assert.Equal(expectedStrength, archer.GetLevelAttributes().Strength);
+            Assert.Equal(expectedDexterity, archer.GetLevelAttributes().Dexterity);
+            Assert.Equal(expectedIntelligence, archer.GetLevelAttributes().Intelligence);
+
+        }
+
+        [Fact]
+        public void ArcherIncreaseLevel_ShouldHaveCorrectIncreasedLevelAttributes()
+        {
+            int expectedLevel = 2;
+            int expectedStrength = 2;
+            int expectedDexterity = 12;
+            int expectedIntelligence = 2;
+
+
+            Hero archer = new Archer("Test Hero");
+            archer.LevelUp();
+
             Assert.Equal(expectedLevel, archer.Level);
             Assert.Equal(expectedStrength, archer.GetLevelAttributes().Strength);
             Assert.Equal(expectedDexterity, archer.GetLevelAttributes().Dexterity);
